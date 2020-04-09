790fa195f475c5d9ede02e82332f66a16bc8a439
Martin Gras <martin.gras95@gmail.com>
2020-04-09 19:35:05 +0100

    form visuals improved contact page re arranged

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import contactStyles from "./contact.module.scss"
-import { Row, Col } from "react-bootstrap"
+import { Row, Col, Form, Button } from "react-bootstrap"
 import SEO from "../components/seo"
 
 const contactPage = () => {
@@ -19,55 +19,37 @@ const contactPage = () => {
                         <h1>Get In Touch</h1>
                     </Col>
                 </Row>
-
+                
                 <Row style={{textAlign:"start",marginLeft:"2rem"}}>
                     <Col md={4} xs={12}>
-                        <h2>Reservations</h2>
-                        <p>gogopizzareserve@gmail.com</p>
-                        <p>+44 73746 892 22</p>
+                        <h2>Question?</h2>
+                        <Form style={{maxWidth:"300px"}} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                            <input type="hidden" name="bot-field" />
+                            <input type="hidden" name="form-name" value="contact" />
+                            <Form.Group controlId="exampleForm.ControlInput1">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="name" placeholder="John Smith" />
+                            </Form.Group>
+                            <Form.Group controlId="exampleForm.ControlInput1">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="name@example.com" />
+                            </Form.Group>
+                            <Form.Group controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Example textarea</Form.Label>
+                                <Form.Control as="textarea" rows="3" placeholder="Type your message here..." />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                            </Form>
                     </Col>
-                    <Col md={4} xs={12}>
-                        <h2>Enquiries</h2>
-                        <p>gogopizzaenquire@gmail.com</p>
-                    </Col>
-                    <Col md={4} xs={12}>
-                    <h2>Address</h2>
-                    <div style={{lineHeight:"4px"}}>
-                        <p>123</p>
-                        <p>Some Street</p>
-                        <p>Edinburgh</p>
-                        <p>EH9 3UJ</p>
-                    </div>
-                </Col>
+                    <Col md={8} xs={12} style={{display:"flex",flexDirection:"column",justifyContent:"center"}}>
+                        <h2>In a hurry? Call Now.</h2>
+                        <h3>+44 73 555 432 299</h3>
+                    </Col>  
                 </Row>
-                <Row>
-                    <Col>
-                        <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">  
-                            <input type="hidden" name="bot-field" />  
-                            <input type="hidden" name="form-name" value="contact" />  
-                            <div className="field half first">
-                                <label htmlFor="name">Name</label>
-                                <input type="text" name="name" id="name" />
-                            </div>
-                            <div className="field half">
-                                <label htmlFor="email">Email</label>
-                                <input type="text" name="email" id="email" />
-                            </div>
-                            <div className="field">
-                                <label htmlFor="message">Message</label>
-                                <textarea name="message" id="message" rows="6" />
-                            </div>
-                            <ul className="actions">
-                                <li>
-                                <input type="submit" value="Send Message" className="special" />
-                                </li>
-                                <li>
-                                <input type="reset" value="Clear" />
-                                </li>
-                            </ul>
-                        </form>
-                    </Col>
-                </Row>
+                
+
 
                 
             </div>
